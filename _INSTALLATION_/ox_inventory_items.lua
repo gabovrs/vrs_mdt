8fa955f4d99e2e44a0f7d472c7ca79b3e6606ca1
Gabriel Varas <gabrielvaras.ibaceta@gmail.com>
2023-12-21 14:38:20 -0300

    create Installation folder

---
+++ _INSTALLATION_/ox_inventory_items.lua
@@ -0,0 +1,11 @@
+return {
+    -- * ONLY IF YOU ARE USING OX_INVENTORY
+    ['mdt'] = {
+		label = 'Police MDT',
+		weight = 500,
+		stack = false,
+		client = {
+			export = 'vrs_mdt.showMDT'
+		}
+	},
+}
