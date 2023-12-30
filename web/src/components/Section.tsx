7f233467ca3051e369872d52abaf0021fabdc075
Gabriel Varas <gabrielvaras.ibaceta@gmail.com>
2023-12-30 02:02:47 -0300

    refactor(web/Section): props types

--- web/src/components/Section.tsx
+++ web/src/components/Section.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { NavLink } from 'react-router-dom';
 
-function Section(props: any) {
+interface SectionProps {
+  link: string;
+  icon: any;
+  title: string;
+}
+
+function Section({ link, icon, title }: SectionProps) {
   return (
     <li>
       <NavLink
-        to={props.link}
-        className={
-          ({ isActive }) => isActive ? 'block rounded p-2 bg-gray-800 text-gray-100' : 
-          'block rounded p-2 hover:text-gray-100 hover:bg-gray-800 transition duration-100'}>
-        <FontAwesomeIcon icon={props.icon} className="fa-fw mr-2" />
-        {props.title}
+        to={link}
+        className={({ isActive }) => isActive ? 'block rounded p-2 bg-gray-800 text-gray-100' : 'block rounded p-2 hover:text-gray-100 hover:bg-gray-800 transition duration-100'}>
+        <FontAwesomeIcon icon={icon} className="fa-fw mr-2" />
+        {title}
       </NavLink>
     </li>
   )
