e967693f2082f002594238fdd31e0768b2ab8b40
Gabriel Varas <gabrielvaras.ibaceta@gmail.com>
2023-12-30 02:03:32 -0300

    feat(web/ImageWithFallback): outline ring option

--- web/src/components/ImageWithFallback.tsx
+++ web/src/components/ImageWithFallback.tsx
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImage } from '@fortawesome/free-solid-svg-icons'
 
-const ImageWithFallback = ({ src, icon, rounded }: any) => {
+interface ImageWithFallbackProps {
+  src: string;
+  icon?: any;
+  rounded?: string;
+  outline?: boolean;
+}
+
+const ImageWithFallback = ({ src, icon, rounded, outline }: ImageWithFallbackProps) => {
   const [imageExists, setImageExists] = useState<boolean>(false);
 
   useEffect(() => {
@@ -31,7 +38,7 @@ const ImageWithFallback = ({ src, icon, rounded }: any) => {
   }, [src])
 
   return imageExists ? (
-    <img src={src} className={`w-full ${rounded ? 'rounded-'+rounded : 'rounded-lg'} aspect-square object-cover`} />
+    <img src={src} className={`w-full ${rounded ? 'rounded-'+rounded : 'rounded-lg'} ${outline && 'outline outline-blue-700 outline-offset-4'} aspect-square object-cover`} />
   ) : (
     <div className={`flex w-full bg-gray-800 aspect-square items-center justify-center ${rounded ? 'rounded-'+rounded : 'rounded-lg'} border border-gray-700 object-cover`}><FontAwesomeIcon icon={icon || faImage} className='text-2xl'/></div>
   )
